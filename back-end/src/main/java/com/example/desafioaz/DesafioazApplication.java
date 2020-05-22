149ac81ef739d0dc7ac2077aad93c5ed1d3696e5
mateus <mateuspaivamatiazzi1@gmail.com>
2020-05-22 17:38:43 -0400

    Criado projeto de back-end

---
+++ back-end/src/main/java/com/example/desafioaz/DesafioazApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioazApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/example/desafioaz/DesafioazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
